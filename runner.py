71ec66f0159bce41b6480ad9e41353e573941c20
michael.tian <michael.tian@loweprofero.com>
2019-05-10 16:38:51 +0800

    Use BeautifulReport replace HTMLTestRunner.

--- runner.py
+++ runner.py
@@ -1,7 +1,8 @@
 """
 Runner
 """
-from HTMLTestRunner import HTMLTestRunner
+#from HTMLTestRunner import HTMLTestRunner
+from BeautifulReport import BeautifulReport
 import unittest
 import time
 import os,sys,getopt
@@ -31,8 +32,12 @@ if __name__ == "__main__":
     #set env
     os.environ["env"] = get_argv(sys.argv[1:])
     now = time.strftime("%Y%m%d-%H%M%S")
-    file_name = "report/{}-result.html".format(now)
+    file_name = "{}-result.html".format(now)
+    """
     with open(file_name, 'wb') as f:
         runner = HTMLTestRunner(stream=f, title="Auto Testing Report", description="Windows 10 Firefox")
         discovr = unittest.defaultTestLoader.discover("./springdemo/test_case", pattern="M*.py")
         runner.run(discovr)
+    """
+    discovr = unittest.defaultTestLoader.discover("./springdemo/test_case", pattern="M*.py")
+    BeautifulReport(discovr).report(filename=file_name, description="Auto Testing Report", log_path="report")
