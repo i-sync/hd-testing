c416a598412f989251e80793fecc43190b1608bb
michael.tian <michael.tian@loweprofero.com>
2019-05-13 15:35:35 +0800

    update selenium webdriver options.

--- driver/browser.py
+++ driver/browser.py
@@ -6,13 +6,13 @@ from selenium import webdriver
 def chrome_browser(headless = None):
     option = webdriver.ChromeOptions()
     option.headless = headless
-    driver = webdriver.Chrome(chrome_options=option)
+    driver = webdriver.Chrome(options=option)
     #driver.maximize_window()
     return driver
 
 def firefox_browser(headless = None):
     option = webdriver.FirefoxOptions()
     option.headless = headless
-    driver = webdriver.Firefox(firefox_options=option)
+    driver = webdriver.Firefox(options=option)
     #driver.maximize_window()
     return driver
