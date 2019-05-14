e28bc5967dfd81e3a4471e66510e6d1f68bff20c
libbyQin <libby.qin@mullenloweprofero.com>
2019-05-14 16:51:49 +0800

    add touring file folder for Tourint project,please run 'python runner.py' to run project

---
+++ touring/page/landing_page.py
@@ -0,0 +1,40 @@
+"""
+Spring Demo Landing Page
+"""
+from page.page import Page
+from springdemo.element.elements_define import ElementsDefine
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.select import Select
+from springdemo.data.urls import current_url
+
+class LandingPage(Page):
+    """
+    Spring Demo Landing Page
+    """
+    def __init__(self, driver):
+        """
+        init function
+        :param driver:
+        """
+        self.url = "/landing"
+        #self.driver = driver
+        #self.base_url = current_url()
+        super(LandingPage, self).__init__(driver, current_url())
+        self.element = ElementsDefine()
+
+    def chose_locale(self, locale):
+        """
+        chose country select locale
+        :param locale:
+        :return:
+        """
+        self.country_select = self.find_element(*self.element.landing_country_select)
+        Select(self.country_select).select_by_value(locale)
+
+    def click_continue_button(self):
+        """
+        click landing page continue button
+        :return:
+        """
+        with self.wait_for_page_load(10):
+            self.find_element(*self.element.landing_continue_button).click()
---
+++ touring/test_case/m0T0100_check_landing_page.py
@@ -0,0 +1,44 @@
+"""
+Spring Demo each locale landing page checking.
+"""
+
+import unittest
+from driver.browser import *
+from touring.page.landing_page import LandingPage
+from BeautifulReport import BeautifulReport
+from touring.data.locales import *
+import time,os
+
+class TestCheckLandingPage(unittest.TestCase):
+    def setUp(self):
+        self.driver = chrome_browser(headless=False)
+        self.landingPage = LandingPage(self.driver)
+    def tearDown(self):
+        self.driver.quit()
+
+    def save_img(self, img_name):
+        """
+            传入一个img_name, 并存储到默认的文件路径下
+        :param img_name:
+        :return:
+        """
+        self.driver.get_screenshot_as_file('{}/{}.png'.format(os.path.abspath(BeautifulReport.img_path), img_name))
+
+    # @unittest.skipUnless(os.environ["env"] == "live", "only check live site")
+    @BeautifulReport.add_test_img(time.strftime("%Y%m%d-%H%M%S")+'_touring_landing_error_img')
+    def test_check_landing_page(self):
+        """
+        Touring
+        Check locale landing page
+        """
+        #check all of the locales
+        for locale in All_Locales:
+            #open landing page
+            self.landingPage.open();
+            #country select
+            self.landingPage.chose_locale(locale)
+            self.assertIn(locale, self.driver.current_url)
+
+
+if __name__ == "__main__":
+    unittest.main()
