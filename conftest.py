3716c1f22ea1ba66f77f5a697fe4125498b9ec10
michael.tian <michael.tian@loweprofero.com>
2019-05-24 17:44:48 +0800

    Update html report utctime to local time.

--- conftest.py
+++ conftest.py
@@ -26,7 +26,7 @@ def pytest_html_results_table_header(cells):
 @pytest.mark.optionalhook
 def pytest_html_results_table_row(report, cells):
     cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+    cells.insert(1, html.td(datetime.now(), class_='col-time'))
     #cells.pop()
 
 @pytest.mark.hookwrapper
