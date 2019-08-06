9598807954d264ced5df0cc29126fb110d305912
michael.tian <michael.tian@loweprofero.com>
2019-08-06 18:32:05 +0800

    Add new matrix v4, and update utils code.

--- proj12_ryi/data/marketmatrix_utils.py
+++ proj12_ryi/data/marketmatrix_utils.py
@@ -7,15 +7,15 @@ __author__ = "Michael.Tian"
 
 import openpyxl
 
-__matrix_file_name__ = "proj12_ryi/data/MY20-RYI-Matrix-V3.1.xlsx"
+__matrix_file_name__ = "proj12_ryi/data/MY20-RYI-Matrix-V4.xlsx"
 
 def get_social_matrix():
     """
     Get all soical link matrix
     locale column B
-    Facebook Url column U
-    Instagram Url column V
-    Twitter Url column W
+    Facebook Url column V
+    Instagram Url column W
+    Twitter Url column X
     :return:
     """
     wb = openpyxl.load_workbook(__matrix_file_name__)
@@ -23,9 +23,9 @@ def get_social_matrix():
     res = {}
     for index in range(4, 37):
         locale = sheet["B{}".format(index)].value.split()[0]
-        facebook = sheet["U{}".format(index)].value
-        instagram = sheet["V{}".format(index)].value
-        twitter = sheet["W{}".format(index)].value
+        facebook = sheet["V{}".format(index)].value
+        instagram = sheet["W{}".format(index)].value
+        twitter = sheet["X{}".format(index)].value
         res[locale] = [facebook, instagram, twitter]
 
     wb.close()
@@ -34,20 +34,20 @@ def get_social_matrix():
 def get_bike_matrix():
     """
     Get All bike info matrix
-    Custom: J,K,L,M,N
-    Performance: O,P,Q
-    Touring: R,S,T
+    Custom: K,L,M,N,O
+    Performance: P,Q,R
+    Touring: S,T,U
     :return:
     """
     wb = openpyxl.load_workbook(__matrix_file_name__)
     sheet = wb["RYI"]
     bike_category = {
-        "custom": ["J", "K", "L", "M", "N"],
-        "performance": ["O", "P", "Q"],
-        "touring": ["R", "S", "T"]
+        "custom": ["K", "L", "M", "N", "O"],
+        "performance": ["P", "Q", "R"],
+        "touring": ["S", "T", "U"]
     }
 
-    # bike_column = ["J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"]
+    # bike_column = ["K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U"]
     bike_column = [col for c in [bike_category[key] for key in bike_category.keys()] for col in c]
     bike_code = {}
     for col in bike_column:
