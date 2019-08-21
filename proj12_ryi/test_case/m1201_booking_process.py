8d584ae817722f3641fa5f2ee5e38be605693b12
michael.tian <michael.tian@loweprofero.com>
2019-08-21 11:19:36 +0800

    update my20 ryi staging url.

--- proj12_ryi/test_case/m1201_booking_process.py
+++ proj12_ryi/test_case/m1201_booking_process.py
@@ -31,10 +31,10 @@ class TestRyiBookingProcess(unittest.TestCase):
         cls.driver.quit()
 
     # @parameterized.expand(['en_EU', 'en_GB', 'en_IE', 'en_ZZ', 'es_ES', 'fi_FI', 'fr_BE', 'fr_CH', 'fr_FR', 'fr_LU'])
-    #@parameterized.expand(['cs_CZ', 'da_DK', 'de_AT', 'de_CH', 'de_DE', 'en_AA', 'en_CA', 'en_EU', 'en_GB', 'en_IE',
-    #                       'en_ZZ', 'es_ES', 'es_MX', 'es_XX', 'fi_FI', 'fr_BE', 'fr_CA', 'fr_CH', 'fr_FR', 'fr_LU',
-    #                       'hu_HU', 'it_CH', 'it_IT', 'nl_BE', 'nl_NL', 'no_NO', 'pl_PL', 'pt_PT', 'ru_RU', 'sv_SE'])
-    @parameterized.expand(['ru_RU'])
+    @parameterized.expand(['cs_CZ', 'da_DK', 'de_AT', 'de_CH', 'de_DE', 'en_AA', 'en_CA', 'en_EU', 'en_GB', 'en_IE',
+                           'en_ZZ', 'es_ES', 'es_MX', 'es_XX', 'fi_FI', 'fr_BE', 'fr_CA', 'fr_CH', 'fr_FR', 'fr_LU',
+                           'hu_HU', 'it_CH', 'it_IT', 'nl_BE', 'nl_NL', 'no_NO', 'pl_PL', 'pt_PT', 'ru_RU', 'sv_SE'])
+    #@parameterized.expand(['ru_RU'])
     def test_ryi_booking_process(self, locale):
         self.test_ryi_dealer_page(locale)
 
