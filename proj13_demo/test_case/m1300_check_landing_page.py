48031c3b01705abbe195915a3adc890d3a14b34d
michael.tian <michael.tian@loweprofero.com>
2019-09-26 15:51:42 +0800

    add my20 demo homepage check.

--- proj13_demo/test_case/m1300_check_landing_page.py
+++ proj13_demo/test_case/m1300_check_landing_page.py
@@ -7,8 +7,8 @@ import unittest
 import pytest
 from driver.browser import *
 
-from proj13_demo.data.locales import *
-from proj13_demo.page.home_page import HomePage
+from proj13_demo.data.marketmatrix_utils import *
+from proj13_demo.page.landing_page import LandingPage
 
 
 @pytest.mark.live_checker
@@ -18,7 +18,7 @@ class TestMY20CheckLandingPage(unittest.TestCase):
     """
     def setUp(self):
         self.driver = firefox_browser()
-        self.homePage = HomePage(self.driver)
+        self.landingPage = LandingPage(self.driver)
 
     def tearDown(self):
         self.driver.quit()
@@ -27,7 +27,23 @@ class TestMY20CheckLandingPage(unittest.TestCase):
         """
         13. my20 Check locale home page
         """
-        pass
+        # get all locales from matrix
+        locales = get_all_locale()
+
+        # check all locale
+        for locale in locales:
+            # open landing page
+            self.landingPage.open()
+            # country select
+            self.landingPage.select_element_by_value(self.landingPage.element.landing_country_select, locale)
+
+            # click submit button
+            self.landingPage.click_element(self.landingPage.element.landing_go_button, refresh_page=True)
+
+            # check home page title
+            page_title = "Harley-Davidson® | Ride Free"
+            self.assertEqual(page_title, self.driver.title,
+                             "My20 Demo Home page title is incorrect: [{}]".format(self.driver.title))
 
 
 if __name__ == "__main__":
